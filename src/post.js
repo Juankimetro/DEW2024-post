f652f6f29b56a283b4a7150af3434a475d0e6c6e
Juankimetro <juanka3027@gmail.com>
2024-10-14 19:07:47 +0100

    Test

--- src/post.js
+++ src/post.js
@@ -1,13 +1,21 @@
 class Post {
   name = ''
   #number = 0
-
+  #subscribers = []
   constructor (name) {
     this.name = name
   }
 
   get summary () {
     return `${this.name} publication number ${this.#number}`
+  }
+
+  getSubscribers () {
+    return this.#subscribers.map(user => user.name).join(',') //
+  }
+
+  attach (user) {
+    this.#subscribers.push(user)
   }
 }
 
